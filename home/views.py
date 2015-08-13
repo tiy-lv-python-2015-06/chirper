4903031124aac4f9720dff51b2daade8218cf917
Jeff <jeffnb@gmail.com>
2015-08-13 06:06:46 -0700

    Adding a simple html css example

--- home/views.py
+++ home/views.py
@@ -6,3 +6,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "home/index.html"
+
+class StarterView(TemplateView):
+    template_name = "home/starting.html"
