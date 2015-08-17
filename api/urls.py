97f70e992f96a12a4b76b013f5b725374f6fcc5c
Jeff <jeff.newburn@theironyard.com>
2015-08-17 16:44:08 -0700

    Adding generic views

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    url(r'^chirps/', ChirpList.as_view()),
-    url(r'^snippets/(?P<pk>[0-9]+)$', ChirpDetailAndUpdate.as_view()),
+    url(r'^chirps/$', ChirpList.as_view()),
+    url(r'^chirps/(?P<pk>[0-9]+)/$', ChirpDetailAndUpdate.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -1,47 +1,21 @@
 from django.contrib.auth.models import User
-from django.shortcuts import render, get_object_or_404
-
-# Create your views here.
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 from updates.models import Chirp
 from updates.serializers import ChirpSerializer
+from rest_framework import permissions
 
-
-class ChirpList(APIView):
+class ChirpList(generics.ListCreateAPIView):
     """
     List All chirps or create a new one
     """
-    def get(self, request, format=None):
-        chirps = Chirp.objects.all()
-        serializer = ChirpSerializer(chirps, many=True)
-        return Response(serializer.data)
+    queryset = Chirp.objects.all()
+    serializer_class = ChirpSerializer
 
-    def post(self, request, format=None):
-        serializer = ChirpSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(author=User.objects.get(pk=1))
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        serializer.save(author=User.objects.get(pk=1))
 
-class ChirpDetailAndUpdate(APIView):
 
-    def get(self, request, pk, format=None):
-        chirp = get_object_or_404(Chirp, pk)
-        serializer = ChirpSerializer(chirp)
-        return Response(serializer.data)
+class ChirpDetailAndUpdate(generics.RetrieveUpdateDestroyAPIView):
 
-    def put(self, request, pk, format=None):
-        chirp = get_object_or_404(Chirp, pk)
-        serializer = ChirpSerializer(chirp)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        chirp = get_object_or_404(Chirp, pk)
-        chirp.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = Chirp.objects.all()
+    serializer_class = ChirpSerializer
