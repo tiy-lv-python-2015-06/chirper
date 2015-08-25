a0b5653a5e7d237620245bd98962c0d265c8b0f6
Jeff <jeff.newburn@theironyard.com>
2015-08-25 10:40:59 -0700

    Fixing static directory

--- chirper/heroku-settings.py
+++ chirper/heroku-settings.py
@@ -23,6 +23,5 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'global')
+    os.path.join(BASE_DIR, 'global'),
 )
