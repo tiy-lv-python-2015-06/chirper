49697a3468bc0332cd055f302d94b2bdb92e7015
Jeff <jeff.newburn@theironyard.com>
2015-08-25 08:45:39 -0700

    Cleaning up project

--- chirper/settings.py
+++ chirper/settings.py
@@ -122,6 +122,7 @@ LOGIN_REDIRECT_URL='/updates/'
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "global"),
@@ -136,7 +137,7 @@ BOOTSTRAP3 = {
     'base_url': '/static/bootstrap/',
 
     # The complete URL to the Bootstrap CSS file (None means no theme)
-    'theme_url': '/static/bootstrap/css/sandstone.css',
+    'theme_url': None,
 
     # Include jQuery with Bootstrap JavaScript (affects django-bootstrap3 template tags)
     'include_jquery': True,
